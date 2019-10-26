3edde1d428f039f3f3854d122e3ed519df7f3c73
HossamAhmedAli <56847513+HossamAhmedAli@users.noreply.github.com>
2019-10-27 00:56:52 +0200

    Add files via upload

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hossam;
+package hossamplay;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,96 +18,26 @@ import javafx.scene.control.TextField;
  * @author Hossam
  */
 public class FXMLDocumentController implements Initializable {
-     public static StringBuffer encrypt(String text, int s) 
-    { 
-        StringBuffer result= new StringBuffer(); 
-  
-        for (int i=0; i<text.length(); i++) 
-        { 
-            if (Character.isUpperCase(text.charAt(i))) 
-            { 
-                char ch = (char)(((int)text.charAt(i) +s -65)%26+65 ); 
-                result.append(ch); 
-            } 
-            else if (Character.isLowerCase(text.charAt(i)))
-            { 
-                char ch = (char)(((int)text.charAt(i) + s -97)% 26 + 97); 
-                result.append(ch); 
-            } 
-            else
-            {
-                char ch='!';
-                result.append(ch);
-            }
-        } 
-        return result; 
-    } 
-     public static StringBuffer decrypt(String text, int s) 
-    { 
-        StringBuffer result= new StringBuffer(); 
-  
-        for (int i=0; i<text.length(); i++) 
-        { 
-            if (Character.isUpperCase(text.charAt(i))) 
-            { 
-                char ch = (char)(((int)text.charAt(i) -s -65)%26+65  ); 
-                result.append(ch); 
-            } 
-            else if (Character.isLowerCase(text.charAt(i)))
-            { 
-                char ch = (char)(((int)text.charAt(i) - s -97)% 26 + 97); 
-                result.append(ch); 
-            } 
-            else
-            {
-                char ch='!';
-                result.append(ch);
-            }
-        } 
-        return result; 
-    } 
-   
-    
-    
-    
+    cryp ob=new cryp();
     
     @FXML
-    private Label label;
+    private Label output;
     @FXML
-    private TextField f1;
+    private TextField plaintext;
     @FXML
-    private Label label2;
-    
-
- 
+    private TextField key;
     
     @FXML
-    private void decrypt(ActionEvent event) {
-       String here= decrypt(label.getText(),1).toString();
-        label2.setText(here);
-    }
-    @FXML
-    private void hi(ActionEvent event) {
-        System.out.println("You clicked me twice!");
-        label2.setText("Hello World number 2!");
-    }
-    @FXML
-    private void encrypt(ActionEvent event) {
-     String here= encrypt(f1.getText(),1).toString();        
-        label.setText(here);      
-    }
-    @FXML
-    private void clear(ActionEvent event) {
-       
-        f1.setText("        ");
-        label2.setText("       ");
-        label.setText("       ");
+    private void handleButtonAction(ActionEvent event) {
         
-    }   
+        ob.setKey(key.getText());
+        ob.KeyGen();
+        output.setText( ob.encryptMessage(plaintext.getText()));
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-        
     }    
     
 }
